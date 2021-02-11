6ed45a05f295c2398749528affdca23c93ea44ac
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2021-02-11 21:44:49 +0300

    Add files via upload

--- My/message.js
+++ My/message.js
@@ -1,15 +1,34 @@
+var modal = document.getElementById("my_modal");
+var btn = document.getElementById("btn_modal_window");
+var span = document.getElementsByClassName("close_modal_window")[0];
 
-function validate(form_id,email) {
+
+function validate(form_id, email) {
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     var address = document.forms[form_id].elements[email].value;
-   
-    if(reg.test(address) == false) {
+
+    if (reg.test(address) == false) {
          alert('Enter the correct e-mail');
          return false;
     }
-    else
-      alert("Thank you, the form has been submitted.")
+    else {
+
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
+
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }   
+    }
 }
-        
+btn.onclick = validate;
+    
 
-
+       
