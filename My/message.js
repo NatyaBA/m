28400c2d15efd0cc38bdb685e99eff4e661cfbe7
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2021-04-03 23:59:36 +0300

    Add files via upload

--- My/message.js
+++ My/message.js
@@ -6,45 +6,38 @@ document.getElementById("clear").onclick = function() {
 }
 
 var modal = document.getElementById("my_modal");
-var btn = document.getElementById("btn_modal_window");
+var btn = document.getElementById("form_id");
 var span = document.getElementsByClassName("close_modal_window")[0];
 
 
 
- function validate(form_id, email) {
+ function validate(formy, email) {
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    var address = document.forms[form_id].elements[email].value;
+    var address = document.forms[formy].elements[email].value;
 
     if (reg.test(address) == false) {
          alert('Enter the correct e-mail');
          return false;
     }
-    else {
+}
+
+btn.onclick = validate;
+btn.addEventListener('click', () => {
         btn.onclick = function () {
             modal.style.display = "block";
-    
         }
 
         span.onclick = function () {
             modal.style.display = "none";
-        
         }
-
+        
         window.onclick = function (event) {
             if (event.target == modal) {
                 modal.style.display = "none";
                
             }
         }   
-       
-    }
-    getId('btn_modal_window').addEventListener('click', () => {
-        setTimeout(f.submit(), 10000);
-    }); 
-}
-btn.onclick = validate;
-
-
+});
    
     
 
